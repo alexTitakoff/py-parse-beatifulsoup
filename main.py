ad0a8b4eff2107780f4c9f6dcc44fe24c93f5d15
Alex Titakoff <titakovad@webinnovations.ru>
2017-11-17 17:13:52 +0300

    python log

--- main.py
+++ main.py
@@ -18,11 +18,13 @@ def get_html(url):
 def get_total_pages(html):
     soup = BeautifulSoup(html, 'lxml')
 
-    pages = soup.find('div', class_='pagination-pages').find_all('a', class_='pagination-page')[-1]
+    pages = soup.find('div', class_='pagination-pages').find_all('a', class_='pagination-page')[-1].get('href')
+    print(pages)
+
 
 def main():
     # https://www.avito.ru/ryazan?s=101&sgtd=1&view=gallery&q=iphone+6
-    pass
+    get_total_pages(get_html('https://www.avito.ru/ryazan?s=101&sgtd=1&view=gallery&q=iphone+6'))
 
 if __name__ == '__main__':
     main()
