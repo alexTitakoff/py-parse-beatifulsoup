6f115b6bca080644de6eea41770dc510c4e4504f
Alex Titakoff <titakovad@webinnovations.ru>
2017-11-17 17:19:18 +0300

    total page func

--- main.py
+++ main.py
@@ -19,12 +19,13 @@ def get_total_pages(html):
     soup = BeautifulSoup(html, 'lxml')
 
     pages = soup.find('div', class_='pagination-pages').find_all('a', class_='pagination-page')[-1].get('href')
-    print(pages)
+    total_pages = pages.split('=')
+    return int(total_pages[1].split('&')[0])
 
 
 def main():
     # https://www.avito.ru/ryazan?s=101&sgtd=1&view=gallery&q=iphone+6
-    get_total_pages(get_html('https://www.avito.ru/ryazan?s=101&sgtd=1&view=gallery&q=iphone+6'))
+    print(get_total_pages(get_html('https://www.avito.ru/ryazan?s=101&sgtd=1&view=gallery&q=iphone+6')))
 
 if __name__ == '__main__':
     main()
